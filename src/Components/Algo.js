5d9f336914509d74a3b61bd8487af8ef9a1f8ae7
antoniouaa <antoniouaa@hotmail.com>
2020-12-16 02:41:48 +0000

    bubblesort donezo

---
+++ src/Components/Algo.js
@@ -0,0 +1,55 @@
+import React from "react";
+import Sketch from "react-p5";
+
+const fisherYatesShuffle = (arr) => {
+  let currentIndex = arr.length;
+  let randomIndex;
+  while (currentIndex !== 0) {
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+    [arr[currentIndex], arr[randomIndex]] = [
+      arr[randomIndex],
+      arr[currentIndex],
+    ];
+  }
+  return arr;
+};
+
+const bubbleSort = (arr) => {
+  for (let i = 0; i < arr.length - 1; i++) {
+    if (arr[i] > arr[i + 1]) {
+      [arr[i], arr[i + 1]] = [arr[i + 1], arr[i]];
+    }
+  }
+};
+
+export default function Algo() {
+  let array = [...Array(100).keys()];
+  let states = [];
+
+  const setup = (p5, canvasParentRef) => {
+    p5.frameRate(22);
+    p5.createCanvas(600, 800).parent(canvasParentRef);
+    fisherYatesShuffle(array);
+  };
+
+  const draw = (p5) => {
+    bubbleSort(array);
+    p5.background(0);
+    p5.fill(255);
+    array.forEach((r, i) => {
+      const h = p5.map(r, 0, 100, 0, 800);
+      const w = 600 / array.length;
+      p5.rect(w * i, 0, w, h);
+    });
+  };
+
+  return (
+    <div>
+      <span>Algo</span>
+      {console.log(states)}
+      <p>{states.length}</p>
+      <Sketch setup={setup} draw={draw} />
+    </div>
+  );
+}
---
+++ src/Components/Title.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Title() {
+  return (
+    <header>
+      <span>Visualisation Tool</span>
+    </header>
+  );
+}
